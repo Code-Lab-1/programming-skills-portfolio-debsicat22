a3a3eccf507cd6497a01efdfc1a6d63dda7638be
Christian Dave Sicat <98599177+debsicat22@users.noreply.github.com>
2023-03-03 16:24:34 +0400

    exercises

---
+++ Chapter 2- Variables & Comments/Exercise 4 Favorite Number.py
@@ -0,0 +1,10 @@
+# Program to print a message revealing a favorite number
+
+# Defining a variable for the favorite number
+favorite_number = 22
+
+# Creating a message using the favorite number variable
+message = "My favorite number is " + str(favorite_number) + "."
+
+# Printing the message
+print(message)
---
+++ Chapter 2- Variables & Comments/Exercise 5 USB Shopper.py
@@ -0,0 +1,17 @@
+# Program to calculate the number of USB sticks that can be purchased for a given amount of money
+
+# Defining the cost of each USB stick
+cost_per_stick = 6
+
+# Defining the amount of money available
+money_available = 50
+
+# Calculating the number of USB sticks that can be purchased
+number_of_sticks = money_available // cost_per_stick
+
+# Calculating the remaining amount of money
+remaining_money = money_available % cost_per_stick
+
+# Printing the number of USB sticks that can be purchased and the remaining amount of money
+print("Number of USB sticks:", number_of_sticks)
+print("Remaining amount of money: £" + str(remaining_money))
